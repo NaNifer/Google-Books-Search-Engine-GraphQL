35b847ad0a226a8f5af4d6b6ad375bd5f9123b29
Nifer Kilakila <nifercritter@gmail.com>
2022-09-16 11:09:51 -0700

    Added authMiddleware to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
-const {ApolloServer} = require('apollo-server-express');
+
+const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
-const {typeDefs, resolvers} = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
+
+// Middleware import
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,9 +15,10 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
-
+// Express middleware for parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -23,17 +27,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
-
+// GraphQL and Express Server start
 db.once('open', async () => {
   await server.start();
-  server.applyMiddleware({app});
-
+  // integrate the Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
 });
+
+
+// Old code from refactoring for MERN:
+// const routes = require('./routes');
+// app.use(routes);
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
